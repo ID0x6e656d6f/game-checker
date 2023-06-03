c921d675e3e024b5a84dee70a84805c1eecafd64
ID0x6e656d6f <114474294+ID0x6e656d6f@users.noreply.github.com>
2023-06-03 13:46:21 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
-$host_name = 'localhost'; //nome dell'host
-$db_user = 'alezag74_gamechecker'; //utente del server
-$db_password = 'zaga2004zz#'; //password dell'utente
+$host_name = '####'; //nome dell'host
+$db_user = '####'; //utente del server
+$db_password = '####'; //password dell'utente
 
 $connection = mysqli_connect($host_name, $db_user, $db_password); //connessione all host
 ?>
